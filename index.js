90c28c686cbf26c62609f6c9a7e56af7bb27a71f
ArjayBuen <arjaybuen10@gmail.com>
2023-07-01 19:59:23 +0800

    adding audio

--- index.js
+++ index.js
@@ -20,7 +20,49 @@ for (let i = 0; i < numberOfDrumButtons; i++) {
     
     document.querySelectorAll(".drum")[i].addEventListener("click",function() {
         // alert("I got clicked!");
-        this.style.color="white";//this = which button is clicked
+        // this.style.color="white";//this = which button is clicked
+
+        var buttonInnerHtml=this.innerHTML;
+        switch (buttonInnerHtml) {
+            case "w":
+                var tom1=new Audio("sounds/tom-1.mp3");
+                tom1.play();
+                console.log(buttonInnerHtml);
+                break;
+            case "a":
+                var tom2=new Audio("sounds/tom-2.mp3");
+                tom2.play();
+                console.log(buttonInnerHtml);
+                break;
+            case "s":
+                var tom3=new Audio("sounds/tom-3.mp3");
+                tom3.play();
+                console.log(buttonInnerHtml);
+                break;
+            case "d":
+                var tom4=new Audio("sounds/tom-4.mp3");
+                tom4.play();
+                console.log(buttonInnerHtml);
+                break;
+            case "j":
+                var snare=new Audio("sounds/snare.mp3");
+                snare.play();
+                console.log(buttonInnerHtml);
+                break;
+            case "k":
+                var crash=new Audio("sounds/crash.mp3");
+                crash.play();
+                console.log(buttonInnerHtml);
+                break;
+            case "l":
+                var kick=new Audio("sounds/kick-bass.mp3");
+                kick.play();
+                console.log(buttonInnerHtml);
+                break;
+            default:
+               console.log(buttonInnerHtml);
+                break;
+        }
     
     });
     
